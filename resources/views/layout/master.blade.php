e24c49ccd9d180994ed4494a51a38cff7b8f220c
Mike Brain <mikeybrain1302@gmail.com>
2018-02-01 01:16:38 +0000

    Viewport

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -3,10 +3,11 @@
     <head> 
         <meta charset="utf-8"> 
         <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+       <meta name="csrf-token" content="{{ csrf_token() }}">
 
         @section('head')
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        
             <link href="{{ asset('css/app.css') }}" rel="stylesheet">
             <link href="{{ asset('css/nav.css') }}" rel="stylesheet">
             <link href="{{ asset('css/footer.css') }}" rel="stylesheet">
